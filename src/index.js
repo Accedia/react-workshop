b8216333478e68c2e1f041ec68fcb92249df9c4c
Ivan Siderov <ivan.siderov@corp.accedia.com>
2018-09-20 16:05:09 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 
 // Note: Change this import if you wish to load a different exercise.
-import CurrentExercise from './exercise_6';
+import CurrentExercise from './exercise_1';
 
 ReactDOM.render(<CurrentExercise />, document.getElementById('root'));
